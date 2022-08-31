a6611e890b9b4bac50d13d29f4fdc6bc8039a964
Dipanshu Jana <dipanshujana64@gmail.com>
2022-08-31 16:15:20 +0530

    1.1

--- PersonalPortfolio/app.js
+++ PersonalPortfolio/app.js
@@ -1,31 +1,13 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const catagoryList = document.getElementsByClassName('catagory-list')[0];
+const imageGalary = document.getElementsByClassName('image-galary')[0];
 const navBar = document.getElementById('nav-right');
-const menuLines = document.querySelectorAll('.line');
-const logo = document.querySelector('.logo');
+let isTrue = false;
 
-const projects = document.getElementById('pr-info');
-const code = document.getElementById('code-info');
-const github = document.getElementById('git-info');
-const awards = document.getElementById('awards-info');
-
-const allBtn = document.getElementById('category-1');
-const techBtn = document.getElementById('category-2');
-const designBtn = document.getElementById('category-3');
-const natureBtn = document.getElementById('category-4');
-
-const allImages = document.getElementById('all');
-const techImages = document.getElementById('technology');
-const designImages = document.getElementById('design');
-const natureImages = document.getElementById('nature');
-
-var isTrue = false;
-
-hamburgerMenu.addEventListener('click', hamburgerMenuFunction);
-
-function hamburgerMenuFunction(){
+hamburgerMenu.addEventListener('click', ()=>{
     if(navBar.style.right = "-100%" && isTrue == false){
         navBar.style.cssText = "display: flex !important";
-        Array.from(menuLines).forEach(element => {
+        Array.from(hamburgerMenu.children).forEach(element => {
             element.style.cssText += "background-color: black;"
         });
         hamburgerMenu.style.cssText += "background-color: white;";
@@ -37,7 +19,7 @@ function hamburgerMenuFunction(){
     }
     
     else if(isTrue){
-        Array.from(menuLines).forEach(element => {
+        Array.from(hamburgerMenu.children).forEach(element => {
             element.style.cssText += "background-color: white;"
         });
         hamburgerMenu.style.cssText += "background-color: black;";
@@ -45,80 +27,71 @@ function hamburgerMenuFunction(){
         navBar.style.animation = "move-right 1s forwards";
         isTrue = false;
     }
-}
+});
 
 (function count(){
-    for (let i = 1; i < 26; i++) {
-        projectsNumCount(i);
-    }
-
-    for (let i = 1; i < 2001; i++) {
-        codesNumCount(i);
-    }
-
-    for (let i = 1; i < 7; i++) {
-        githubNumCount(i);
-    }
-
-    for (let i = 1; i < 11; i++) {
-        awardsNumCount(i);
-    }
+    for (let i = 1; i < 26; i++) {projectsNumCount(i);}
+    for (let i = 1; i < 2001; i++) {codesNumCount(i);}
+    for (let i = 1; i < 7; i++) {githubNumCount(i);}
+    for (let i = 1; i < 11; i++) {awardsNumCount(i);}
 
     function projectsNumCount(i){
         setTimeout(() => {
-            projects.innerText = i + "+";
+            document.getElementById('pr-info').innerText = i + "+";
         }, 500*i);
     }
 
     function awardsNumCount(i){
         setTimeout(() => {
-            awards.innerText = i + "+";
+            document.getElementById('awards-info').innerText = i + "+";
         }, 500*i);
     }
 
     function codesNumCount(i){
         setTimeout(() => {
-            code.innerText = i + "+";
+            document.getElementById('code-info').innerText = i + "+";
         }, 5*i);
     }
 
     function githubNumCount(i){
         setTimeout(() => {
-            github.innerText = i + "+";
+            document.getElementById('git-info').innerText = i + "+";
         }, 500*i);
     }
 }());
 
 (function showImage(){
-    allBtn.addEventListener('click', ()=>{
-        allImages.style.display = "grid";
-        techImages.style.display = "none";
-        designImages.style.display = "none";
-        natureImages.style.display = "none";
-    });
-    
-    techBtn.addEventListener('click', ()=>{
-        techImages.style.display = "grid";
-        allImages.style.display = "none";
-        designImages.style.display = "none";
-        natureImages.style.display = "none";
-    });
-
-    designBtn.addEventListener('click', ()=>{
-        designImages.style.display = "grid";
-        allImages.style.display = "none";
-        techImages.style.display = "none";
-        natureImages.style.display = "none";
-    });
-
-    natureBtn.addEventListener('click', ()=>{
-        natureImages.style.display = "grid";
-        allImages.style.display = "none";
-        techImages.style.display = "none";
-        designImages.style.display = "none";
-    });
+    Array.from(catagoryList.children).forEach((button)=>{
+        button.addEventListener('click', (e)=>{
+            Array.from(imageGalary.children).forEach((imageDiv)=>{imageDiv.style.display = "none";})
+            if (e.target.id === 'category-1'){
+                document.getElementById('all').style.display = "grid";
+            }
+            else if (e.target.id === 'category-2'){
+                document.getElementById('technology').style.display = "grid";
+            }
+            else if (e.target.id === 'category-3'){
+                document.getElementById('design').style.display = "grid";
+            }
+            else {
+                document.getElementById('nature').style.display = "grid";
+            }
+        })
+    })
 }());
 
-logo.addEventListener('click', ()=>{
+(function clearValues(){
+    document.getElementById('submit-btn').addEventListener('click', ()=>{
+        document.getElementById('name-area').value = "";
+        document.getElementById('msg-area').value = "";
+        document.getElementById('email-area').value = "";
+
+        setTimeout(() => {
+            alert('Your response has been submitted sucessfully!');
+        }, 100);
+    })
+}());
+
+document.querySelector('.logo').addEventListener('click', ()=>{
     location.reload();
 })
